5a609a882036985a64ecc33d6eea405729f8d7c2
Shrut <shrutpatel20@gmail.com>
2024-06-13 23:37:45 -0500

    add sidebar hover styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import "./main.css";
 
-console.log('hey');
+// const dropdown = document.querySelector('#project-drop');
+// const projectAll = document.querySelector('.project-all');
+
+// dropdown.addEventListener('click', ()=>{
+//     dropdown.classList.toggle('rotated');
+//     projectAll.classList.toggle('hidden');
+// });
+
